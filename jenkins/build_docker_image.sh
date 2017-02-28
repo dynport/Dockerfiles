a0f563e593c56409d77cc0cd38f6c8c74bf2a7fc
Tobias Schwab <tobias.schwab@dynport.de>
2017-02-28 15:50:49 +0100

    add improve script to build jenkins image
    * download most recent kc release from github releases
    * add kc release version to image tag

---
+++ jenkins/build_docker_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e -o pipefail
+
+KC_VERSION=v14
+VERSION=$(grep "FROM jenkinsci" Dockerfile |  cut -d ":" -f 2)
+TAG=260336115275.dkr.ecr.eu-west-1.amazonaws.com/dynport/jenkins:${VERSION}-kc-${KC_VERSION}
+
+function abort() {
+  echo $@
+  exit 1
+}
+
+function download_kc_release {
+  token=$(git config --global --get github.token)
+  if [[ -z $token ]]; then
+    abort "github token must be present in .gitconfig"
+  fi
+
+  url=$(curl -s -u :${token} https://api.github.com/repos/dynport/kc/releases | jq '.[0].assets[] | select(.name | contains("linux.amd64")) | .url' -c -r)
+
+  if [[ -z $url ]]; then
+    abort "unable to extract asset url"
+  fi
+
+  curl -sL -H 'Accept: application/octet-stream' -u :${token} ${url} | tar xfz -
+}
+
+if [[ -z $VERSION ]]; then
+  abort "unable to extract VERSION from Dockerfile"
+fi
+
+d=$(mktemp -d)
+cp Dockerfile $d
+pushd $d > /dev/null
+trap "rm -Rf $d" EXIT
+
+download_kc_release
+
+
+echo building ${TAG}
+docker build -t ${TAG} .
+docker push ${TAG}
+echo built ${TAG}
