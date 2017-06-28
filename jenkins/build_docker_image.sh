e6a9baa91f4a97306bf16c479e95420f6cdd3b14
Tobias Schwab <tobias.schwab@dynport.de>
2017-06-28 13:41:52 +0200

    make docker_build_image.sh more flexible

--- jenkins/build_docker_image.sh
+++ jenkins/build_docker_image.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-set -e -o pipefail
-
-KC_VERSION=v19
-VERSION=$(grep "FROM jenkinsci" Dockerfile |  cut -d ":" -f 2)
-TAG=260336115275.dkr.ecr.eu-west-1.amazonaws.com/dynport/jenkins:${VERSION}-kc-${KC_VERSION}
-
 function abort() {
   echo $@
   exit 1
 }
 
+GITHUB_TOKEN=${GITHUB_TOKEN:-$(git config --get github.token)}
+KC_VERSION=${KC_VERSION:-$(curl -s -u :${GITHUB_TOKEN} "https://api.github.com/repos/dynport/kc/releases" | jq '.[0] | .tag_name' -c -r)}
+
+set -e -o pipefail
+
+if [[ -z $GITHUB_TOKEN ]]; then
+    abort "GITHUB_TOKEN must be set"
+fi
+
+if [[ -z $KC_VERSION ]]; then
+  abort "unable to get kc version"
+fi
+
+echo "using kc version ${KC_VERSION}"
+
+VERSION=$(grep "FROM jenkinsci" Dockerfile |  cut -d ":" -f 2)
+TAG=260336115275.dkr.ecr.eu-west-1.amazonaws.com/dynport/jenkins:${VERSION}-kc-${KC_VERSION}
+
 function download_kc_release {
-  token=$(git config --global --get github.token)
-  if [[ -z $token ]]; then
+  if [[ -z $GITHUB_TOKEN ]]; then
     abort "github token must be present in .gitconfig"
   fi
 
-  url=$(curl -s -u :${token} https://api.github.com/repos/dynport/kc/releases | jq '.[0].assets[] | select(.name | contains("linux.amd64")) | .url' -c -r)
+  url=$(curl -s -u :${GITHUB_TOKEN} https://api.github.com/repos/dynport/kc/releases | jq '.[0].assets[] | select(.name | contains("linux.amd64")) | .url' -c -r)
 
   if [[ -z $url ]]; then
     abort "unable to extract asset url"
   fi
 
-  curl -sL -H 'Accept: application/octet-stream' -u :${token} ${url} | tar xfz -
+  curl -sL -H 'Accept: application/octet-stream' -u :${GITHUB_TOKEN} ${url} | tar xfz -
 }
 
 if [[ -z $VERSION ]]; then
