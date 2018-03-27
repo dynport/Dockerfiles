b1c5dc7e8fffe974b3edc13ba8334d13b036cb03
Tobias Schwab <tobias.schwab@dynport.de>
2018-03-27 22:45:24 +0200

    jenkins-agent: add script to build docker images with gcloud

---
+++ jenkins-agent/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -o pipefail
+
+export GITHUB_TOKEN=$(kubectl get secrets/build-github-token -o json  | jq '.data.token' -c -r | base64 -d)
+export IMAGE=eu.gcr.io/build-140318/jenkins-agent:v8
+export KC_VERSION=v27
+
+if [[ -z $GITHUB_TOKEN ]]; then
+  echo "unable to find GITHUB_TOKEN"
+  exit 1
+fi
+
+
+DOWNLOAD_URL=$(curl -SsfL -u :${GITHUB_TOKEN} https://api.github.com/repos/dynport/kc/releases | jq '.[] | .assets[] | select(.name == "kc.'${KC_VERSION}'.linux.amd64.tgz") | .url'  -c -r)
+
+curl -s -H 'Accept: application/octet-stream' -u :${GITHUB_TOKEN} -L ${DOWNLOAD_URL} | tar xfz -
+echo "downloaded kc"
+trap "rm -f kc" EXIT
+
+gcloud container builds submit --substitutions=_GITHUB_TOKEN=${GITHUB_TOKEN},_IMAGE=${IMAGE} --config build.yml .
+echo "pushed ${IMAGE}"
